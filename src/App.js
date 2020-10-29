6abca941aa23bd28ae44c4585de52dbf34878f61
yphanikumar2869 <yphanikumar58@gmail.com>
2020-10-29 18:58:08 +0530

    my second changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>My First React App in Github Pages</h1>
       <h2>Author Name is: Y Phani Kumar.</h2>
+      <h3>Learning React js.</h3>
     </div>
   );
 }
