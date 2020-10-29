20f44b74982368c40efdf6d871cd09dedf95d9d9
yphanikumar2869 <yphanikumar58@gmail.com>
2020-10-29 17:30:29 +0530

    Deployed my first react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My First React App in Github Pages</h1>
+      <h2>Author Name is: Y Phani Kumar.</h2>
+    </div>
+  );
+}
+
+export default App;
